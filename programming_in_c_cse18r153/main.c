f9be0353cc3847e6e49310ae54a2292e842c2650
Dharanidharan2001 <53188699+Dharanidharan2001@users.noreply.github.com>
2019-08-10 00:29:36 -0700

    Add files via upload

--- programming_in_c_cse18r153/main.c
+++ programming_in_c_cse18r153/main.c
@@ -10,8 +10,14 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    printf("Hello World dharanidharan");
-
-
+    int num1,num2;
+    printf("enter two numbers");
+    scanf("%d%d",&num1,&num2);
+    if(num1>num2)
+    printf("%d is maximum",num1);
+    else if(num1<num2)
+    printf("5d is maximum",num2);
+    else if(num1==num2)
+    printf("both are equal");
     return 0;
 }
