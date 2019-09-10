c576158526ff289d93bd4561278bb98f360c520d
Dharanidharan2001 <53188699+Dharanidharan2001@users.noreply.github.com>
2019-09-10 20:26:49 +0530

    post increment program

---
+++ main.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void main()
+{
+    int a=1,b=1,c;
+    c=a++ +b;
+    printf("%d%d",a,b);
+}
