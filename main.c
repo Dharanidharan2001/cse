4f817b8ded6acee1b2e2ded0a0a29f759628a764
Dharanidharan2001 <53188699+Dharanidharan2001@users.noreply.github.com>
2019-10-18 17:22:50 +0200

    Maximum of array

--- main.c
+++ main.c
@@ -7,9 +7,38 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-void main()
+
+int main()
 {
-    int a=1,b=1,c;
-    c=a++ +b;
-    printf("%d%d",a,b);
+    printf("Hello World");
+
+    return 0;
+}
+#include <stdio.h>
+ 
+int main()
+{
+  int array[100], maximum, size, c, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &size);
+ 
+  printf("Enter %d integers\n", size);
+ 
+  for (c = 0; c < size; c++)
+    scanf("%d", &array[c]);
+ 
+  maximum = array[0];
+ 
+  for (c = 1; c < size; c++)
+  {
+    if (array[c] > maximum)
+    {
+       maximum  = array[c];
+       location = c+1;
+    }
+  }
+ 
+  printf("Maximum element is present at location %d and it's value is %d.\n", location, maximum);
+  return 0;
 }
