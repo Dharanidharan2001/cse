9df261428ff4fb289d1f5ae7908b41e03e98bd4d
Dharanidharan2001 <53188699+Dharanidharan2001@users.noreply.github.com>
2020-06-06 08:32:53 +0530

    are of cylinder

---
+++ python/area of cylinder.py
@@ -0,0 +1,12 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:40:30) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> base=float(input("enter the base:"))
+enter the base:23
+>>> height=float(input("enter the height"))
+enter the height12
+>>> area of cylinder=base*height
+SyntaxError: invalid syntax
+>>> area=base*height
+>>> print(area)
+276.0
+>>> 
