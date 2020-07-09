eac4b8599d23bcd1c7a5430566550003a7a1777c
Dharanidharan <dharanidharanashok@gmail.com>
2020-07-09 07:45:41 +0530

    anagram of two strings

--- python/anagram.py
+++ python/anagram.py
@@ -1,40 +1,38 @@
-def isAnagram(str1, str2):  
+def isAnagram(str1, str2):
 
-    n1 = len(str1)  
+    n1 = len(str1)
 
-    n2 = len(str2)  
+    n2 = len(str2)
 
-    
 
-    if n1 != n2:  
+
+    if n1 != n2:
 
         return 0
 
 
-    str1 = sorted(str1) 
+    str1 = sorted(str1)
 
-    str2 = sorted(str2)  
+    str2 = sorted(str2)
 
-  
 
-    for i in range(0, n1):  
-    
-        if str1[i] != str2[i]:  
+
+    for i in range(0, n1):
+
+        if str1[i] != str2[i]:
 
             return 0
 
-  
-
-    return 
+     return
 
 str1 = "lion"
 
 str2 = "noil"
 
-if isAnagram(str1, str2):  
+if isAnagram(str1, str2):
 
-    print ("The two strings are anagram of each other") 
+    print ("The two strings are anagram of each other")
 
-else:  
+else:
 
-    print ("The two strings are not anagram of each other") 
+    print ("The two strings are not anagram of each other")
