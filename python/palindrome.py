a04604dee4627233ad1451057b41709998daa712
Dharanidharan <dharanidharanashok@gmail.com>
2020-07-10 09:27:46 +0530

    palindrome

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,6 +1,6 @@
 word=input("enter the word")
 rev=word[::-1]
 if word==rev:
-    print("it is a palindrome")
+    print("It is a palindrome")
 else:
-    print("not a palindrome")
+    print("Not a palindrome")
