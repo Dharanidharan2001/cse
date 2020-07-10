cf3b6f3b70c718fd3ee6a5f57964214072d9ca55
Dharanidharan <dharanidharanashok@gmail.com>
2020-07-10 09:34:37 +0530

    password craker

---
+++ python/pass_guess.py
@@ -0,0 +1,12 @@
+from random import *
+guess=""
+password=input("password:")
+letters=["1","2","3","4","5","6","7","8","9","0",a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+while (guess!=password):
+    guess= ""
+    for letter in password:
+        guess_letter=letters[randint(0,25)]
+        guess=str(guess_letter)+str(guess)
+    print(guess)
+print("password guessed")
+input("")
