227a683b31efe32f6ce60c9f41021afbcf0490b1
dharanidharan <dharanidharanashok@gmail.com>
2020-08-06 11:53:32 +0530

    More readable

--- python/count_zeroes_ones.py
+++ python/count_zeroes_ones.py
@@ -2,14 +2,14 @@ def count(arr):
     # Write your code here
     count0 = 0
     count1 = 0
-    for x in arr:
-        for y in x:
-            if y == 0:
+    for row in arr:
+        for element in row:
+            if element == 0:
                 count0 += 1
-            if y == 1:
+            if element == 1:
                 count1 += 1
     print(count0)
-    print(count1)        
+    print(count1)
 
 input=[[1,0,0,1],[0,1,0,1],[1,1,0,1]]
 count(input)
