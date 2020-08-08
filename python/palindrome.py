cd3c75359aa2bd712c1800a767abcd2a3638d4f7
dharanidharan <dharanidharanashok@gmail.com>
2020-08-08 07:27:21 +0530

    palindrome

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,8 +1,8 @@
 def palindrome(word):
-    # word=input("enter the word")
     rev=word[::-1]
     if word==rev:
         print("It is a palindrome")
     else:
         print("Not a palindrome")
+
 palindrome("car")
