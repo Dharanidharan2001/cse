c47a29f07054e8ebaba192c6774c4456f495d32f
dharanidharan <dharanidharanashok@gmail.com>
2020-08-09 18:01:52 +0530

    transpose

--- python/transpose_of_a_matrix.py
+++ python/transpose_of_a_matrix.py
@@ -1,13 +1,15 @@
-arr= [[1,2,3,4],[2,3,4,5],[4,5,7,5],[2,8,9,8]]
-b=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+def transpose(arr):
 
+    for i in range(len(arr)):
+       for j in range(len(arr)):
+          b[i][j] = arr[j][i]
+          print(b[i][j],end=' ')
+       print()
+
+#using nested list
 # m = [[arr[j][i] for j in range(len(arr))] for i in range(len(arr[0]))]
 # for r in m:
 #     print(r)
-
-
-for i in range(len(arr)):
-    for j in range(len(arr)):
-        b[i][j] = arr[j][i]
-        print(b[i][j],end=' ')
-    print()
+b=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+arr=[[1,2,3,2],[3,4,2,1],[5,3,2,1],[6,7,5,3]]
+transpose(arr)
