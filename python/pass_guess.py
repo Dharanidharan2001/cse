c46dcdf14e2f4b1475d436fa30f08a7c1f51bfbe
dharanidharan <dharanidharanashok@gmail.com>
2021-01-07 14:28:07 +0530

    password checker

--- python/pass_guess.py
+++ python/pass_guess.py
@@ -1,12 +1,17 @@
 from random import *
-guess=""
-password=input("password:")
-letters=["1","2","3","4","5","6","7","8","9","0",a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-while (guess!=password):
-    guess= ""
-    for letter in password:
-        guess_letter=letters[randint(0,25)]
-        guess=str(guess_letter)+str(guess)
-    print(guess)
-print("password guessed")
-input("")
+
+def checker(password):
+
+    guess=""
+    #password="hell"
+    letters=["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    while (guess!=password):
+        guess= ""
+        for letter in password:
+            guess_letter=letters[randint(0,25)]
+            guess=str(guess_letter)+str(guess)
+        print(guess)
+    print("password guessed")
+
+password=input("enter:")
+print(checker(password))
