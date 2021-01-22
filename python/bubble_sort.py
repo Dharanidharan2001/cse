ba85ee09974ff94ec89aa5b35514cd91a89b84f1
dharanidharan <dharanidharanashok@gmail.com>
2021-01-22 08:54:44 +0530

    sorting

---
+++ python/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble(items):
+    for i in range(len(items)):
+        already_sorted = True
+        for j in range(len(items)-i-1):
+            if items[j] > items[j+1]:
+                items[j],items[j+1]=items[j+1],items[j]
+                already_sorted= False
+    return items
+
+items=[2,1,4,3,6,5]
+print(bubble(items))
