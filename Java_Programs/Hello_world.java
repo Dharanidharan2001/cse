8402a83158b996ed359b9d4bd47e6f2f10a7d6ad
Dharanidharan <dharanidharanashok@gmail.com>
2023-09-08 15:19:14 +0530

    updated

--- Java_Programs/Hello_world.java
+++ Java_Programs/Hello_world.java
@@ -1,4 +1,7 @@
+package Java_Programs;
+
 class hello{
     public static void main(String args[]){
-        System.out.println("hello world this is dharani")    }
+        System.out.println("hello world this is dharani");
+        System.out.println("this is my first java commit");    }
 }
