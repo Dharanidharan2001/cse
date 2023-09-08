b78359bd01a34fa62930563cc0c8c3549967fa10
Dharanidharan <dharanidharanashok@gmail.com>
2023-09-08 21:54:45 +0530

    with user input

--- Java_Programs/Hello_world.java
+++ Java_Programs/Hello_world.java
@@ -6,5 +6,6 @@ class hello{
     public static void main(String args[]){
         Scanner inp = new Scanner(System.in);
         System.out.println("hello world this is "+ inp.nextLine());
-        System.out.println("this is my first java commit");    }
+        System.out.println("this is my first java commit");  
+        inp.close();  }
 }
