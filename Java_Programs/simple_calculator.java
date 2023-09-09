63600f8c8f63202b36aae3e438a170ffb38e0f92
Dharanidharan <dharanidharanashok@gmail.com>
2023-09-09 09:16:40 +0530

    updated simple calci with switch case

--- Java_Programs/simple_calculator.java
+++ Java_Programs/simple_calculator.java
@@ -8,16 +8,42 @@ class Calculator{
 
         Scanner Uval =  new Scanner(System.in);
         
-        double fnum,snum,output;
+        double num1,num2;
+        String operator;
 
         System.out.println("enter the first num");
-        fnum = Uval.nextDouble();
+        num1 = Uval.nextDouble();
         System.out.println("enter the second num");
-        snum = Uval.nextDouble();
+        num2 = Uval.nextDouble();
 
-        output = fnum+snum;
-        System.out.println(output);
+        System.out.println("enter the operator");
+        operator = Uval.next();
         
+        double result = 0;
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Error: cannot divide by 0");
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("Error: invalid operator");
+        }
+        
+        System.out.println("The result is: " + result);
 
+    
+        Uval.close();
     }
 }
